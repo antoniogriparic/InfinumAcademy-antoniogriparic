5c314b92734715dd62c72b4d1703ddadc4f385c0
antoniogriparic <antonio.griparic@gmail.com>
2021-07-23 16:23:42 +0200

    add homework-03 extra part

--- TV Shows/Login/LoginViewController.swift
+++ TV Shows/Login/LoginViewController.swift
@@ -6,9 +6,6 @@
 //
 
 import UIKit
-import SVProgressHUD
-import Alamofire
-
 
 final class LoginViewController : UIViewController {
     
@@ -26,6 +23,7 @@ final class LoginViewController : UIViewController {
     
     private var rememberMeButtonEnabled = false
     private var visibleButtonEnabled = false
+    private var userService = UserService()
     
     // MARK: - Lifecycle methods -
     
@@ -76,13 +74,13 @@ final class LoginViewController : UIViewController {
     
     @IBAction func loginButtonHandler() {
         guard let email = usernameTextField.text, let password = passwordTextField.text else { return }
-        loginUserWith(email: email, password: password)
+        userService.loginUserWith(email: email, password: password)
         navigateToHomeScreen()
     }
     
     @IBAction func registerButtonHandler() {
         guard let email = usernameTextField.text, let password = passwordTextField.text else { return }
-        registerUserWith(email: email, password: password)
+        userService.registerUserWith(email: email, password: password)
         navigateToHomeScreen()
     }
     
@@ -166,79 +164,6 @@ private extension LoginViewController {
         let storyboard = UIStoryboard(name: "Home", bundle: nil)
         let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
         navigationController?.pushViewController(homeViewController, animated: true)
-    }
-    
-    
-}
-
-
-private extension LoginViewController {
-    func registerUserWith(email : String , password : String) {
-        
-        SVProgressHUD.show()
-        
-        let parametars: [String: String] = [
-            "password" : password,
-            "email" : email,
-            "password_confirmation" : password
-        ]
-        
-        AF
-            .request(
-                "https://tv-shows.infinum.academy/users",
-                method: .post,
-                parameters: parametars,
-                encoder: JSONParameterEncoder.default
-            )
-            .validate()
-            .responseDecodable(of: UserResponse.self) { response in
-                switch response.result {
-                case .success(let userResponse):
-                    print(userResponse)
-                    self.loginUserWith(email: email , password : password)
-                case .failure(let error):
-                    SVProgressHUD.showError(withStatus: "Error")
-                    print("Error : \(error)")
-                }
-            }
-    }
-    
-    func loginUserWith(email : String, password : String) {
-        
-        SVProgressHUD.show()
-        
-        let parametars : [String: String] = [
-            "email" : email,
-            "password" : password
-        ]
-        
-        AF
-            .request(
-                "https://tv-shows.infinum.academy/users/sign_in",
-                method: .post,
-                parameters: parametars,
-                encoder: JSONParameterEncoder.default
-            )
-            .validate()
-            .responseDecodable(of: UserResponse.self) { [weak self] response in
-                switch response.result {
-                case .success(let userResponse):
-                    let headers = response.response?.headers.dictionary ?? [:]
-                    self?.handleSuccesfulLogin(user: userResponse.user, headers: headers)
-                case .failure(let error):
-                    print(error)
-                    SVProgressHUD.showError(withStatus: "Error")
-                }
-            }
-    }
-    
-    func handleSuccesfulLogin(user: User, headers: [String: String]) {
-        guard let authInfo = try? AuthInfo(headers: headers) else {
-            SVProgressHUD.showError(withStatus: "Missing Headers")
-            return
-        }
-        print(authInfo)
-        SVProgressHUD.showSuccess(withStatus: "Success")
     }
     
     
--- TV Shows/Login/User.swift
+++ TV Shows/Login/User.swift
@@ -58,4 +58,5 @@ struct AuthInfo: Codable {
         }
     }
     
+    
 }
